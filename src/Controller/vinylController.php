833b1033ca9b5c25585647d66f8cf15853d95d97
Mariem Ridene <ridenemariem18@gmail.com>
2024-02-27 11:07:06 +0100

    Added some Tiwggy goodness

--- src/Controller/vinylController.php
+++ src/Controller/vinylController.php
@@ -2,13 +2,28 @@
 namespace App\Controller;
 
 use function Symfony\Component\String\u;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class vinylController{
+class vinylController extends AbstractController{
     #[Route("/page1")]
     function homepage():Response{
-        die("bonjour <strong>DSI22!</strong>");
+
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig',[
+            'title'=>'Iset Kelibia',
+            'tracks' => $tracks,
+        ]);
 
     }
     #[Route("/browse/{slug}")]
